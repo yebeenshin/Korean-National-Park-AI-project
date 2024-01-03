712f9a5e95feb751d91ae30b20c655b2624dd571
yebeenshin <56160835+yebeenshin@users.noreply.github.com>
2024-01-03 19:12:54 +0900

    Update change_detection_2.py

--- streamlit/page/change_detection_2.py
+++ streamlit/page/change_detection_2.py
@@ -27,8 +27,6 @@ def app():
     # folium 지도 객체에 Earth Engine 레이어 추가 메서드를 연결
     folium.Map.add_ee_layer = change.add_ee_layer
 
-    ee.Initialize()    # GEE API를 초기화합니다.
-
     # V-World 타일 서비스 URL (API 키 포함)
     vworld_satellite_url = "http://api.vworld.kr/req/wmts/1.0.0/{api_key}/Satellite/{z}/{y}/{x}.jpeg"
     vworld_hybrid_url = "http://api.vworld.kr/req/wmts/1.0.0/{api_key}/Hybrid/{z}/{y}/{x}.png"
@@ -52,8 +50,6 @@ def app():
                 2. 'submit' 버튼을 클릭하면 식생지수 분석 시작!
                 3. 10년간의 식생지수를 분석하여 미래 식생지수를 예측하고, 식생지수 추이를 보여줍니다.
                 """)
-
-    ee.Initialize()    # GEE API를 초기화합니다.
 
     tab1, tab2 = st.tabs(['국립공원 선택', 'GeoJson 파일 업로드'])
 
